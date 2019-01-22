c7e71d0412037962719985180eca4eee8d9d7944
Dustin Brickwood <dustinbrickwood204@gmail.com>
2019-01-22 17:50:06 -0500

    removed docker pointing to internal dir for api and ui

--- shyft-config/shyft-cli/startShyftGeth.sh
+++ shyft-config/shyft-cli/startShyftGeth.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if [ -z "${DBENV}" ]; then
-  ./build/bin/geth --config ./shyft-config/config.toml --ws --nat=any --mine --minerthreads 4 --unlock "0xb276840e8b89c64b517629de60de861e85f539ca" --password ./shyft-config/unlockPasswords.txt
+  ./build/bin/geth --config ./shyft-config/config.toml --ws --nat=any --mine --minerthreads 4
 else
-  /bin/geth --config ./shyft-config/docker-config.toml --ws --nat=any --mine --minerthreads 4 --unlock "0xb276840e8b89c64b517629de60de861e85f539ca" --password ./shyft-config/unlockPasswords.txt
+  /bin/geth --config ./shyft-config/docker-config.toml --ws --nat=any --mine --minerthreads 4
 fi
