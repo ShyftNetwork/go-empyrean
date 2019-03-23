971657c8e975dc84f455a3b2fdfe8d4219c5c9c8
Gregory Markou <gregorymarkou@gmail.com>
2019-03-23 18:21:29 -0400

    remove pg stuff

--- shyft-config/shyft-cli/setup.sh
+++ shyft-config/shyft-cli/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set +e
-dropdb 'shyftdb'
+# dropdb 'shyftdb'
 sh ./shyft-config/shyft-cli/resetShyftGeth.sh &&                     # Reset geth data - Remove pg and chain data
 sh ./shyft-config/shyft-cli/initShyftGeth.sh                         # Init Shyft Geth
