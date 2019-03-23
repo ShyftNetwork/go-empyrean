9c2ea488905acfd80163395bdd50b4d3a8bbc4ea
Gregory Markou <gregorymarkou@gmail.com>
2019-03-23 11:57:03 -0400

    cleaned up scripts

--- shyft-config/shyft-cli/startShyftGeth.sh
+++ shyft-config/shyft-cli/startShyftGeth.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if [ -z "${DBENV}" ]; then
-  ./build/bin/geth --config ./shyft-config/config.toml --ws --nat=any --mine --minerthreads 4
+  ./build/bin/geth --config ./shyft-config/config.toml --ws --nat=any --mine --minerthreads 4 --disablepg --unlock "0xd255b42bc8307199ecc3ffe8055e18cee49eb386" --password ./shyft-config/unlockPasswords.txt
 else
-  /bin/geth --config ./shyft-config/docker-config.toml --ws --nat=any --mine --minerthreads 4
+  /bin/geth --config ./shyft-config/docker-config.toml --ws --nat=any --mine --minerthreads 4 --disablepg --unlock "0xd255b42bc8307199ecc3ffe8055e18cee49eb386" --password ./shyft-config/unlockPasswords.txt
 fi
