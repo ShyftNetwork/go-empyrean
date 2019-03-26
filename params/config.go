937bbf7f5b7c9c3e50db9608c4ee05391820c07d
Dustin Brickwood <dustinbrickwood204@gmail.com>
2019-03-26 11:38:29 -0500

    fixed start script for rpc enabled

--- params/config.go
+++ params/config.go
@@ -108,7 +108,7 @@ var (
 		DAOForkBlock:        nil,
 		DAOForkSupport:      true,
 		EIP150Block:         big.NewInt(2),
-		EIP150Hash:          common.HexToHash("0x9b095b36c15eaf13044373aef8ee0bd3a382a5abb92e402afa44b8249c3a90e9"),
+		EIP150Hash:          common.HexToHash(""),
 		EIP155Block:         big.NewInt(3),
 		EIP158Block:         big.NewInt(3),
 		ByzantiumBlock:      big.NewInt(1035301),
--- shyft-config/shyft-cli/startShyftGeth.sh
+++ shyft-config/shyft-cli/startShyftGeth.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if [ -z "${DBENV}" ]; then
-  ./build/bin/geth --config ./shyft-config/config.toml --ws --nat=any --mine --minerthreads 4
+  ./build/bin/geth --config ./shyft-config/config.toml --ws --rpc --nat=any --mine --minerthreads 4
 else
-  /bin/geth --config ./shyft-config/docker-config.toml --ws --nat=any --mine --minerthreads 4
+  /bin/geth --config ./shyft-config/docker-config.toml --ws --rpc --nat=any --mine --minerthreads 4
 fi
