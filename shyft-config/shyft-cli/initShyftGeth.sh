c642e8ea7ae2b3cfc1850aebf1c1692bdeb64694
Chris <chris@shyft.network>
2019-06-07 15:39:38 -0400

    [ disabled pg from setup ]

--- shyft-config/shyft-cli/initShyftGeth.sh
+++ shyft-config/shyft-cli/initShyftGeth.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 if [ -z "${DBENV}" ]; then
-  ./build/bin/geth --identity "ShyftTestnetNode" --keystore ./ --datadir "./shyftData" init ./shyft-config/ShyftNetwork.json
+  ./build/bin/geth --disablepg --identity "ShyftTestnetNode" --keystore ./ --datadir "./shyftData" init ./shyft-config/ShyftNetwork.json
 else
   if [ -d /go-empyrean/shyftData/geth/chaindata ]; then
     echo "Skipping Genesis Initialization as already completed"
     :
   else
     echo "Initializing Custom Genesis Block"
-    /bin/geth --identity "ShyftTestnetNode" --datadir "./shyftData" init ./shyft-config/ShyftNetwork.json
+    /bin/geth --disablepg --identity "ShyftTestnetNode" --datadir "./shyftData" init ./shyft-config/ShyftNetwork.json
   fi
 fi
